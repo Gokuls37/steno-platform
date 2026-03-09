1f7935bc64c04a88d6fc86427695ee9c1880c7d9
Gokuls37 <hargokul003@gmail.com>
2026-03-09 11:49:06 +0000

    g

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "SSC Steno Platform Running"
+def typing():
+    return render_template("typing_test.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
